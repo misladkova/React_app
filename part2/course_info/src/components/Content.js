a764e47799238f633614fef63ed326925e7792e5
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-11-04 00:42:56 +0100

    extend content component

--- part2/course_info/src/components/Content.js
+++ part2/course_info/src/components/Content.js
@@ -3,9 +3,11 @@ import Part from "./Part";
 
 const Content = (props) => {
     console.log(props)
+    const total = props.parts.reduce((sum, x)=>sum+x.exercises, 0)
     return (
         <div>
             {props.parts.map(x=><Part key={x.id} name={x.name} exercises={x.exercises}/>)}
+            <p>total of {total} exercises</p>
         </div>
     )
 }
