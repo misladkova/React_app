f09d045e1fa03019f76d4b84e7d5d5ea648c056c
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-11-03 23:05:14 +0100

    define part component

--- part2/course_info/src/components/Part.js
+++ part2/course_info/src/components/Part.js
@@ -1,1 +1,15 @@
 import React from 'react'
+
+const Part = (props) => {
+    console.log(props)
+    return(
+        <div>
+            <p>
+                {props.name} {props.exercises}
+            </p>
+        </div>
+    )
+}
+
+export default Part
+
