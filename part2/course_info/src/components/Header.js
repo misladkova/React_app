a19394d04c9c514a178cb4985f6a9d096aa54ed1
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-11-03 23:39:30 +0100

    update header component

--- part2/course_info/src/components/Header.js
+++ part2/course_info/src/components/Header.js
@@ -4,7 +4,7 @@ const Header = (props) => {
     console.log(props)
     return (
         <div>
-            <h1> {props.course.name} </h1>
+            <h1> {props.name} </h1>
         </div>
     )
 }
