75d8bbd3dc3c0521fb50bd568d0e79f40a72327c
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-11-03 23:43:03 +0100

    update content component

--- part2/course_info/src/components/Content.js
+++ part2/course_info/src/components/Content.js
@@ -1,1 +1,13 @@
 import React from 'react'
+import Part from "./Part";
+
+const Content = (props) => {
+    console.log(props)
+    return (
+        <div>
+            {props.parts.map(x=><Part key={x.id} name={x.name} exercises={x.exercises}/>)}
+        </div>
+    )
+}
+
+export default Content
