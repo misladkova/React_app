3e27cfcc0928f82ab666959affbc023c4789156e
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-11-04 16:59:08 +0100

    implement addition of a new person

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,23 +3,37 @@ import Person from "./components/Person";
 
 const App = () => {
     const [ persons, setPersons ] = useState([
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas'}
     ])
     const [ newName, setNewName ] = useState('')
+
+    const addName = (event) =>{
+        event.preventDefault()
+        const personObj = {name: newName}
+        setPersons(persons.concat(personObj))
+        setNewName('')
+    }
+
+    const handleNameChange = (event) =>{
+        console.log(event.target.value)
+        setNewName(event.target.value)
+    }
 
     return (
         <div>
             <h2>Phonebook</h2>
-            <form>
+            <form onSubmit={addName}>
                 <div>
-                    name: <input />
+                    name: <input value={newName} onChange={handleNameChange}/>
                 </div>
                 <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
-            ...
+            <ul>
+                {persons.map(person=> <Person name={person.name}/>)}
+            </ul>
         </div>
     )
 }
