1c76224a84a5ed8847f371daccfd0323e8f07a5d
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-11-05 20:21:38 +0100

    add filter

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Persons from "./components/Persons";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
+import axios from 'axios'
 
 const App = () => {
-    const [ persons, setPersons ] = useState([
-        { name: 'Arto Hellas', number: '032-9876544', id: 1}
-    ])
+    const [ persons, setPersons ] = useState([])
+    //     { name: 'Arto Hellas', number: '032-9876544', id: 1}
+    // ])
     const [ newFilter, setNewFilter ] = useState('')
+
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response=>{
+                console.log('promise fulfilled')
+                setPersons(response.data)
+            })
+    }, [])
+    console.log('render', persons.length, 'persons')
 
     return (
         <div>
