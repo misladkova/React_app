30a5cd514af569db3504ae04b8997a855f845fd9
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-11-05 21:02:36 +0100

    add file

--- part2/data_countries/src/App.js
+++ part2/data_countries/src/App.js
@@ -1,13 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () =>{
 
+    const [countries, setCountries] = useState([])
     const [filter, setFilter] = useState('')
 
     const handleChange = (event) =>{
         console.log(event.target.value)
         setFilter(event.target.value.toLowerCase())
     }
+
+    useEffect(() =>{
+        console.log('effect')
+        axios
+            .get('https://restcountries.eu/rest/v2/all')
+            .then(response=>{
+                console.log('promise fulfilled')
+                setCountries(response.data)
+            })
+    }, [])
+    console.log('render', countries.length, 'countries')
 
     return(
         <div>
