f5bb9d27b9701585f8fc0e3b9eb146d51c89d68b
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-11-06 23:29:31 +0100

    refactor

--- part2/data_countries/src/App.js
+++ part2/data_countries/src/App.js
@@ -1,42 +1,46 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import Country from "./components/Country";
 
-const App = () =>{
+const App = () => {
 
     const [countries, setCountries] = useState([])
     const [filter, setFilter] = useState('')
+    const [display, setDisplay] = useState('')
 
-    const handleChange = (event) =>{
+    const handleChange = (event) => {
         console.log(event.target.value)
         setFilter(event.target.value.toLowerCase())
+        setDisplay('')
     }
 
-    useEffect(() =>{
+    useEffect(() => {
         console.log('effect')
         axios
             .get('https://restcountries.eu/rest/v2/all')
-            .then(response=>{
+            .then(response => {
                 console.log('promise fulfilled')
                 setCountries(response.data)
             })
     }, [])
     console.log('render', countries.length, 'countries')
 
-    const filteredCountries = () =>{
-        const filtered = countries.filter(country=>country.name.toLowerCase().includes(filter))
-            if(filtered.length>1 && filtered.length<=10) {
-                return <ul>{filtered.map(country => <li>{country.name}</li>)}</ul>
-            }
-            if(filtered.length>10) {
-                return 'Too many matches, specify another filter'
-            }else{
-                return filtered.map(country=><div><h2>{country.name}</h2> capital {country.capital}<br />
-                    population {country.population} <h3>languages</h3> <li>{}</li>
-                    <img src={country.flag} alt={'flag'} width={500} height={400}/></div>)
-            }
+
+    const filteredCountries = () => {
+        const filtered = countries.filter(country => country.name.toLowerCase().includes(filter))
+        if(display !== ''){
+            console.log(filtered[0])
+            return <Country country={display} setDisplay={setDisplay} show={true}/>
+        }
+        if (filtered.length > 10) {
+            return 'Too many matches, specify another filter'
+        }
+        else if ((filtered.length > 1 && filtered.length <= 10)) {
+            return filtered.map(country => <Country country={country} setDisplay={setDisplay} show={false}/>)
+        }
     }
 
-    return(
+    return (
         <div>
             find countries <input value={filter} onChange={handleChange}/>
             {filteredCountries()}
