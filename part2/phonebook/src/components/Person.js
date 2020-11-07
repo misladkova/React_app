c1902b96cfd00ebaf5799056e7af553992b0c3cb
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-11-07 20:44:55 +0100

    expand

--- part2/phonebook/src/components/Person.js
+++ part2/phonebook/src/components/Person.js
@@ -1,8 +1,22 @@
 import React from "react";
+import personService from '../services/persons'
 
 const Person = (props) =>{
+    console.log("aaaa", props)
+    const handleClick = () =>{
+        let result = window.confirm(`Delete ${props.name}?`)
+        if(result===true){
+            console.log('h', props)
+            personService.removePersonServer(props.id)
+                .then(response=>{
+                    console.log('k', response)
+                    let aaa = props.persons.filter(x=>x.id!==props.id)
+                    props.setPersons(aaa)
+                })
+        }
+    }
     return(
-        <li>{props.name} {props.number}</li>
+        <li>{props.name} {props.number}<button onClick={handleClick}>delete</button> </li>
     )
 }
 
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -5,7 +5,8 @@ const Persons = (props) => {
     const personsFiltered = props.persons.filter(person=>person.name.toLowerCase().includes(props.newFilter))
     return(
         <ul>
-            {personsFiltered.map(person=> <Person key={person.id} name={person.name} number={person.number}/>)}
+            {personsFiltered.map(person=> <Person key={person.id} name={person.name} number={person.number}
+            setPersons={props.setPersons} id={person.id} persons={props.persons}/>)}
         </ul>
     )
 }
