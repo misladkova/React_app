61f8ecb2cad017469188bf73fffd99748f7384a4
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-11-13 00:02:32 +0100

    add file

---
+++ part4/blogList/app.js
@@ -0,0 +1,27 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('Connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, {
+    useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false,
+    useCreateIndex: true
+})
+    .then(result=>{
+        logger.info('Connected to MongoDB')
+    })
+    .catch(error=>{
+        logger.error('Error while connecting to MongoDB: ',error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
