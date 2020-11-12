961e809bc13b6fc160b1be341bb66eee797881e0
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-11-12 19:05:58 +0100

    refactor

--- part4/blogList/index.js
+++ part4/blogList/index.js
@@ -1,20 +1,9 @@
-const http = require('http')
+// const http = require('http')
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+const Blog = require('./models/blog')
 
 app.use(cors())
 app.use(express.json())
@@ -29,7 +18,6 @@ app.get('/api/blogs', (request, response) => {
 
 app.post('/api/blogs', (request, response) => {
     const blog = new Blog(request.body)
-
     blog
         .save()
         .then(result => {
