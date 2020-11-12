6ee1bb6d33c2acf564936bff6c718e8c183839c2
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-11-12 02:13:06 +0100

    remove redundant lines

--- part3/secondPhonebook/index.js
+++ part3/secondPhonebook/index.js
@@ -1,4 +1,3 @@
-// const http = require('http')
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
@@ -11,10 +10,6 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan('combined'))
 
-// const app = http.createServer((request, response) =>{
-//     response.writeHead(200, {'Content-Type': 'application/json'})
-//     response.end(JSON.stringify(persons))
-// })
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello world</h1>')
@@ -43,13 +38,6 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    // const id = Number(req.params.id)
-    // const x = persons.find(person=>person.id===id)
-    // if(x){
-    //     res.json(x)
-    // }else {
-    //     res.status(404).end()
-    // }
     Person.findById(req.params.id)
         .then(person => {
             if (person) {
@@ -65,9 +53,6 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    // const id = Number(req.params.id)
-    // persons = persons.filter(person => person.id !== id)
-    // res.status(204).end()
     Person.findByIdAndRemove(req.params.id)
         .then(person => {
             Person.find({}).then(persons => {
@@ -79,13 +64,6 @@ app.delete('/api/persons/:id', (req, res) => {
 
 app.post('/api/persons', (req, res) => {
     const body = req.body
-    // const id = Math.floor(Math.random() * Math.floor(10000))
-    // console.log('b', body)
-    // const newPerson = {
-    //     id: id,
-    //     name: body.name,
-    //     number: body.number
-    // }
     Person.find({name: req.body.name}).then(aa => {
         if(aa.length === 0){
             const person = new Person({
@@ -107,42 +85,20 @@ app.post('/api/persons', (req, res) => {
             })
         }
     })
-    // person.save().then(person => {
-    //     Person.find({}).then(persons => {
-    //         res.json(persons)
-    //     })
-    // const x = persons.find(x=>x.name===body.name)
-    // if(x!==undefined){
-    //     res.status(400).json({
-    //         error: 'name must be unique'
-    //     })
-    // }else if(body.name==="" || body.number===""){
-    //     res.status(400).json({
-    //         error: 'both name and number are required'
-    //     })
-    // }else {
-    //     persons = persons.concat(newPerson);
-    //     res.json(persons)
-    // }
 })
 
 app.put('/api/persons/:id', (req, res) => {
     Person.find({name: req.body.name}).then(aa => {
-        console.log("niecp", aa)
         if(aa.length === 0){
             return;
         }
         const clovek = aa[0]
         clovek.number = req.body.number
         clovek.save().then(aaa => {
-            console.log("nieco2", aaa)
-            const output = {"data": clovek};
-            res.json(output)
+            res.json(clovek)
         })
     })
 })
-// najdeme si konkretneho cloveka
-// if existuje tak ho zmenime
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
