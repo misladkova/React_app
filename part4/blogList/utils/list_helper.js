46d6da5c46f863c22a00b060318a5a0ba30d1eb5
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-11-13 16:49:37 +0100

    expand

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -8,6 +8,24 @@ const totalLikes = (blogs) =>{
     }
 }
 
+const favoriteBlog = (blogs)=>{
+    if(blogs.length===0){
+        return []
+    }else if(blogs.length===1){
+        return {title: blogs[0].title, author: blogs[0].author, likes: blogs[0].likes}
+    }else {
+        const newObj = blogs.reduce((largest, curr)=>{
+            if(curr.likes>largest.likes){
+                return curr
+            }else{
+                return largest
+            }
+        })
+        return {title: newObj.title, author: newObj.author, likes: newObj.likes}
+    }
+}
+
 module.exports = {
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
