f4951130fb11625ce652e5a8780175105207b9d4
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-11-13 16:50:36 +0100

    add file

---
+++ part4/blogList/tests/favoriteBlog.test.js
@@ -0,0 +1,47 @@
+const listHelper = require('../utils/list_helper')
+
+describe('favoriteBlog', ()=>{
+    test('when list is empty, result equals zero', ()=>{
+        const blogs = []
+        const result = listHelper.favoriteBlog(blogs)
+        expect(result).toEqual([])
+    })
+    test('when list contains one blog, result equals the likes of that', ()=>{
+        const blogs = [
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+                likes: 5,
+                __v: 0
+            }
+        ]
+        const result = listHelper.favoriteBlog(blogs)
+        const expected = {title: 'Go To Statement Considered Harmful', author: 'Edsger W. Dijkstra', likes: 5}
+        expect(result).toEqual(expected)
+    })
+    test('when list contains more than one, find out which one has most likes', ()=>{
+        const blogs = [
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+                likes: 5,
+                __v: 0
+            },
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+                likes: 10,
+                __v: 0
+            }
+        ]
+        const res = listHelper.favoriteBlog(blogs)
+        const expected = {title: 'Go To Statement Considered Harmful', author: 'Edsger W. Dijkstra', likes: 10}
+        expect(res).toEqual(expected)
+    })
+})
