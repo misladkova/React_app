2549e0ce3464080d434c9b1484e04e19e9b30fef
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-11-15 15:13:04 +0100

    update

--- part4/blogList/app.js
+++ part4/blogList/app.js
@@ -6,6 +6,7 @@ const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 logger.info('Connecting to', config.MONGODB_URI)
 
@@ -25,5 +26,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
