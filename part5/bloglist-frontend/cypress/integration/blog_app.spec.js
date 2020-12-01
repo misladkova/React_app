d831dc7ddc08be8cb5562eaa398b44c408d4a03d
Michaela Sladkova <michaela.sladkova@gmail.com>
2020-12-01 22:26:25 +0100

    add tests

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,16 +1,32 @@
 describe('Blog app', function () {
-    beforeEach(function (){
+    beforeEach(function () {
         cy.visit('http://localhost:3000')
     })
 
-    it('loginForm can be opened', function (){
+    it('loginForm is shown', function () {
         cy.contains('Log in').click()
     })
-    it('user can log in', function (){
-        cy.contains('Log in').click()
-        cy.get('#username').type('bmurph')
-        cy.get('#password').type('bmurph')
-        cy.get('#login-button').click()
-        cy.contains('Ben Murph is logged in')
+
+    describe('Login', function () {
+        it('successful login', function () {
+            cy.contains('Log in').click()
+            cy.get('#username').type('bmurph')
+            cy.get('#password').type('bmurph')
+            cy.get('#login-button').click()
+            cy.contains('Ben Murph is logged in')
+        })
+    })
+
+    describe('When user is logged in', function (){
+        beforeEach(function (){
+            cy.contains('Log in').click()
+            cy.get('#username').type('bmurph')
+            cy.get('#password').type('bmurph')
+            cy.get('#login-button').click()
+            cy.contains('Ben Murph is logged in')
+        })
+        it('A new blog can be created', function (){
+
+        })
     })
 })
